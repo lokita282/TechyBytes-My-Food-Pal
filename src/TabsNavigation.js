e611cfe7c7381cd120a21e39a6388d85c64db880
unknown <priyambi.hiran@gmail.com>
2022-01-29 10:51:10 +0530

    Bottom Tab navigation set up

--- src/TabsNavigation.js
+++ src/TabsNavigation.js
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LandingPage from './screens/LandingPage';
+import Dashboard from './screens/Dashboard';
+import Upload from './screens/Upload';
+import Chatbot from './screens/Chatbot';
+import MonthReport from './screens/MonthReport';
+import BottomTab from './screens/BottomTab';
 
 
 
@@ -10,9 +15,13 @@ function TabsNavigation() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='LandingPage'>
-        <Stack.Screen name='LandingPage' component={LandingPage} options={{ headerShown: false }}>
-        </Stack.Screen>
+      <Stack.Navigator initialRouteName='BottomTab'>
+        <Stack.Screen name='LandingPage' component={LandingPage} options={{ headerShown: false }}> </Stack.Screen>
+        <Stack.Screen name='BottomTab' component={BottomTab} options={{ headerShown: false }}> </Stack.Screen>
+        <Stack.Screen name='Dashboard' component={Dashboard} options={{ headerShown: false }}> </Stack.Screen>
+        <Stack.Screen name='MonthReport' component={MonthReport} options={{ headerShown: false }}> </Stack.Screen>
+        <Stack.Screen name='Chatbot' component={Chatbot} options={{ headerShown: false }}> </Stack.Screen>
+        <Stack.Screen name='Upload' component={Upload} options={{ headerShown: false }}> </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ src/screens/MonthReport.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MonthReport = () => {
+  return <div></div>;
+};
+
+export default MonthReport;
